7123359fd59c8bc17f1576610362c749fbaf7572
max söderquist <macintosh100@hotmail.com>
2021-05-16 12:40:13 +0200

    Add movieDetail component

---
+++ src/components/MovieDetail/MovieDetail.js
@@ -0,0 +1,21 @@
+import "./MovieDetail.css";
+
+const MovieDetail = ({Poster, Title, Type, Year, imdbID}) => {
+    return (
+        <div className="MovieDetail">
+            <img
+                className="MovieDetail__Poster"
+                src={Poster}
+                alt="poster"/>
+            <div className="MovieDetail__Details">
+                <h2 className="MovieDetail__Title">{Title}</h2>
+                <h3 className="MovieDetail__Type">{Type}</h3>
+                <h3 className="MovieDetail__Year">{Year}</h3>
+                <p className="MovieDetail__ID">{imdbID}</p>
+                <button className="MovieDetail__Fave">Add to favorites</button>
+            </div>
+        </div>
+    )
+}
+
+export default MovieDetail;
